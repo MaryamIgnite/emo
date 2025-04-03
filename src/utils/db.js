9d67ae8851bd3b1b36a5d1ef38d0818c2883434f
MaryamIgnite <maryam.ahmad@ignitetech.com>
2025-04-03 22:33:59 +0500

    Update db.js

--- src/utils/db.js
+++ src/utils/db.js
@@ -1,10 +1,10 @@
 import mysql from "mysql2/promise";
 
 const pool = mysql.createPool({
-  host: "localhost", // Change this if your database is hosted elsewhere
-  user: "root", // Your MySQL username
-  password: "Temp/2109", // Your MySQL password
-  database: "meo", // Your database name
+  host: "sql8.freesqldatabase.com", // Change this if your database is hosted elsewhere
+  user: "sql8771105", // Your MySQL username
+  password: "L1tlIDmimf", // Your MySQL password
+  database: "sql8771105", // Your database name
   waitForConnections: true,
   connectionLimit: 10,
   queueLimit: 0
